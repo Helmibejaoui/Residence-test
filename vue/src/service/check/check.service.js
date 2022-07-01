27d3ff5cacdb222c91b8b325c38558522555abcc
helmi.bejaoui <helmi.bejaoui@mazars.fr>
2022-07-01 18:38:40 +0100

    change url

--- vue/src/service/check/check.service.js
+++ vue/src/service/check/check.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "../auth/auth-header";
 
-let url = 'http://ip172-18-0-42-cavipdejd6ng0099ljm0-8000.direct.labs.play-with-docker.com/';
+let url = 'http://ip172-18-0-42-cavipdejd6ng0099ljm0-8000.direct.labs.play-with-docker.com';
 
 class CheckService {
 
